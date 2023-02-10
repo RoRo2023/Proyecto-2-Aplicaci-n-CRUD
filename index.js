99417ee7d6eb089319c994fbbdc432ac9356e09a
RoRo2023 <119353807+RoRo2023@users.noreply.github.com>
2023-02-09 18:26:00 -0600

    Responsive

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
             nombres.push([nuevoCorreo,nuevaDescripcion]);
             localStorage.setItem('nombres', JSON.stringify(nombres));
 
-            content.innerHTML = '<tr><th class="c1">Email </th><th class="c2">Descripción </th><th class="c3">Estado </th><th class="c4">Eliminar </th><th class="c5">Editar </th></tr>';
+            content.innerHTML = '<tr><th class="c1">Email </th><th class="c2">Descripción </th><th class="c3">Estado </th><th class="c4">Eliminar</th><th class="c5">Editar </th><th class="c6">Acciones</th></tr>';
 
             for(let i = 0; i < nombres.length; i++){
                 showNames(i, nombres);
@@ -67,6 +67,8 @@ document.addEventListener("DOMContentLoaded", function () {
         td4.className ="c4";
         const td5 = document.createElement('td');
         td5.className ="c5";
+        const td6 = document.createElement('td'); //Columna invisible
+        td6.className ="c6";
         
         const td1text = document.createTextNode(nombres[i][0]);
         const td2text = document.createTextNode(nombres[i][1]);
@@ -87,9 +89,17 @@ document.addEventListener("DOMContentLoaded", function () {
         del.onclick = () => {
             eliminar(i)
         }
-    
         td4.appendChild(del);
-    
+        
+        //Botón eliminar de la columna invisible
+        const del2 = document.createElement('button');
+        del2.innerText = "Eliminar";
+        del2.classList = "Eliminar btn3";
+        del2.onclick = () => {
+            eliminar(i)
+        }
+        td6.appendChild(del2);
+
         //Botón editar
         const edit = document.createElement('button');
         edit.innerText = "Editar";
@@ -121,7 +131,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     nombres.splice(i, 1, [input_email.value, input_desc.value]);
                     localStorage.setItem('nombres', JSON.stringify(nombres));
 
-                    content.innerHTML = '<tr><th class="c1">Email </th><th class="c2">Descripción </th><th class="c3">Estado </th><th class="c4">Eliminar </th><th class="c5">Editar </th></tr>';
+                    content.innerHTML = '<tr><th class="c1">Email </th><th class="c2">Descripción </th><th class="c3">Estado </th><th class="c4">Eliminar</th><th class="c5">Editar </th><th class="c6">Acciones</th></tr>';
 
                     for(let i = 0; i < nombres.length; i++){
                         showNames(i, nombres);
@@ -135,14 +145,61 @@ document.addEventListener("DOMContentLoaded", function () {
             form.appendChild(button_save);
             button_save.id = i;
         }
-    
         td5.appendChild(edit);
     
+        //Botón editar de la columna invisible
+        const edit2 = document.createElement('button');
+        edit2.innerText = "Editar";
+        edit2.classList = "edit btn3"
+        edit2.onclick = () => {
+            input_email.value = nombres[i][0];
+            input_desc.value = nombres[i][1];
+
+            button_save.className = "btn1";
+            button_submit.className = "invisible";
+
+
+            button_save.onclick = () => {
+                nombres[i][0] = input_email.value;
+                nombres[i][1] = input_desc.value;
+
+                
+                //Verificación
+                if (input_desc.value == ""|| input_email.value == ""){
+                    alert("Datos incompletos. Por favor llene ambos campos.");
+
+                }else if (!input_email.value.includes("@")){
+                    alert("Correo inválido");
+        
+                }else if(input_desc.value.length<20){
+                    alert("Descripción demasiado corta");
+
+                }else{
+                    nombres.splice(i, 1, [input_email.value, input_desc.value]);
+                    localStorage.setItem('nombres', JSON.stringify(nombres));
+
+                    content.innerHTML = '<tr><th class="c1">Email </th><th class="c2">Descripción </th><th class="c3">Estado </th><th class="c4">Eliminar</th><th class="c5">Editar </th><th class="c6">Acciones</th></tr>';
+
+                    for(let i = 0; i < nombres.length; i++){
+                        showNames(i, nombres);
+                    }
+
+                    button_save.className = "invisible";
+                    button_submit.className = "btn1";
+                }           
+            }
+
+            form.appendChild(button_save);
+            button_save.id = i;
+        }
+        td6.appendChild(edit2);
+
         tr.appendChild(td1);
         tr.appendChild(td2);
         tr.appendChild(td3);
         tr.appendChild(td4);
         tr.appendChild(td5);
+        tr.appendChild(td6);
     
         content.appendChild(tr);
         //La tabla  = "content"
@@ -156,9 +213,9 @@ document.addEventListener("DOMContentLoaded", function () {
         localStorage.setItem('nombres', JSON.stringify(nombres));
 
         if (nombres.length>0) {
-        content.innerHTML = '<tr><th class="c1">Email </th><th class="c2">Descripción </th><th class="c3">Estado </th><th class="c4">Eliminar </th><th class="c5">Editar </th></tr>';
+            content.innerHTML = '<tr><th class="c1">Email </th><th class="c2">Descripción </th><th class="c3">Estado </th><th class="c4">Eliminar</th><th class="c5">Editar </th><th class="c6">Acciones</th></tr>';
         }else{
-            content.innerHTML = '<tr><th class="c1">Email </th><th class="c2">Descripción </th><th class="c3">Estado </th><th class="c4">Eliminar </th><th class="c5">Editar </th></tr><p>No hay elementos</p>';
+            content.innerHTML = '<tr><th class="c1">Email </th><th class="c2">Descripción </th><th class="c3">Estado </th><th class="c4">Eliminar</th><th class="c5">Editar </th><th class="c6">Acciones</th></tr><p>No hay elementos</p>';
         }
 
         for(let j = 0; j < nombres.length; j++){
